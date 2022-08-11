f3df3fbb7a445cff98a7c44b5ef421f6ab3b5b68
HugoRodrigues <hugordsp@gmail.com>
2022-08-10 21:41:16 -0300

    Usando o Jinja2 com .html

--- app.py
+++ app.py
@@ -2,11 +2,11 @@
 import os
 os.system('cls')
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask("projeto")
 
 @app.route ("/")
 def ola_mundo ():
-    return "Olá mundo! Esse é meu primeiro projeto Flask!!", 200
+    return render_template("alo.html"), 200
 
 app.run()
